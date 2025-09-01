3a211efd775ed13541881f37cb8a08c3d3763186
Mikhail Sergeev <server-15@yandex.ru>
2025-09-01 06:10:46 +0500

    [del] status model

--- addons/rental/models/__init__.py
+++ addons/rental/models/__init__.py
@@ -1,7 +1,6 @@
 from . import vehicle_type
 from . import vehicle_model
 from . import vehicle
-from . import maintenance_status
 from . import maintenance_log
 from . import order
 from . import office
