e55ea26b757bad00f962501dc93b2137ff8872ba
Mikhail Sergeev <server-15@yandex.ru>
2026-05-24 13:12:45 +0400

    [UPD] context

--- addons/rental_vehicles/models/res_users.py
+++ addons/rental_vehicles/models/res_users.py
@@ -20,8 +20,16 @@ class ResUsers(models.Model):
     def context_get(self):
         user = self.env.user
         ctx = dict(super().context_get())
+        
+        office_id = False
+        
+        if user.office_id:
+            office_id = user.office_id.id
+        elif user.office_ids:
+            office_id = user.office_ids[:1].id
+        
         ctx.update({
             "allowed_office_ids": user.office_ids.ids if hasattr(user, "office_ids") else [],
-            "office_id": user.office_ids[:1].id if user.office_ids else False,
+            "office_id": office_id
         })
         return frozendict(ctx)
